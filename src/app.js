cb66b5030bf8de14b9bbca8732df095b912a4450
NaveenS <s.naveen759@gmail.com>
2020-09-03 17:33:22 +0530

    deploying in website server

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -102,6 +103,6 @@ app.get('*',(request, response) => {
     })
 })
 
-app.listen('3000',() => {
-    console.log("the server is running in the port 3000")
+app.listen(port,() => {
+    console.log("the server is running in the port " + port)
 })
